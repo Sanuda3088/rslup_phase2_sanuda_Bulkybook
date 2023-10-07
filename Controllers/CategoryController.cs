f950f2ece63a2faafc9cda0b86ee2ed6a1b0494d
Sanuda3088 <sanudasenod446@gmail.com>
2023-10-07 18:47:25 +0530

    after 1.46 hours

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Bulkybook.Data;
+using Bulkybook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulkybook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationdbContext _db;
+
+        public CategoryController(ApplicationdbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
